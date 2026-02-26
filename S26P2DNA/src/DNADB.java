8931242bcac7b2e013c8b63adb3b9f4fd8eaf4cc
calilystar <63565179+Calilystar@users.noreply.github.com>
2026-02-25 20:05:31 -0500

    files

---
+++ S26P2DNA/src/DNADB.java
@@ -0,0 +1,111 @@
+// -------------------------------------------------------------------------
+
+/**
+ * The database implementation for this project.
+ * This manages the commands for the DNA tree.
+ *
+ * @author CS3114/5040 Staff
+ * @version Spring 2026
+ *
+ */
+public class DNADB implements DNA {
+
+    // ----------------------------------------------------------
+    /**
+     * Create a new DNADB object.
+     */
+    public DNADB() {
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Insert a DNA string into the database
+     * 
+     * @param sequence
+     *            The sequence to insert
+     * @return The outcomes message string
+     */
+    public String insert(String sequence) {
+        if (sequence == null) {
+            return "Bad input: Sequence may not be null\r\n";
+        }
+        else if (sequence.equals("")) {
+            return "Bad input: Sequence may not be empty\r\n";
+        }
+        else {
+            for (int i = 0; i < sequence.length(); i++) {
+                if (sequence.charAt(i) != 'A' || sequence.charAt(i) != 'G'
+                    || sequence.charAt(i) != 'C' || sequence.charAt(i) != 'T') {
+                    return "Bad Input Sequence |" + sequence + "|\r\n";
+                }
+            }
+        }
+        return null;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Remove a DNA string into the database
+     * 
+     * @param sequence
+     *            The sequence to remove
+     * @return The outcomes message string
+     */
+    public String remove(String sequence) {
+        return null;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Print the tree
+     * 
+     * @return the print string
+     */
+    public String print() {
+        return null;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Print the lengths
+     * 
+     * @return the print string
+     */
+    public String printLengths() {
+        return null;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Print the stats
+     * 
+     * @return the print string
+     */
+    public String printStats() {
+        return null;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Search for a given string
+     * 
+     * @param sequence
+     *            The sequence to search for
+     * @return the print string
+     */
+    public String search(String sequence) {
+        for (int i = 0; i < sequence.length(); i++) {
+            if (sequence.charAt(i) != 'A' || sequence.charAt(i) != 'G'
+                || sequence.charAt(i) != 'C' || sequence.charAt(i) != 'T') {
+                return "Bad Input Sequence |" + sequence + "|\r\n";
+            }
+        }
+        return null;
+    }
+}
