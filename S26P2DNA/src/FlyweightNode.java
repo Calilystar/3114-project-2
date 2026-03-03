b52c0b0606cb8b7effe908bcc3ae0eed66174c0e
calilystar <63565179+Calilystar@users.noreply.github.com>
2026-03-02 23:01:35 -0500

    maybe the current basis

---
+++ S26P2DNA/src/FlyweightNode.java
@@ -0,0 +1,40 @@
+
+public class FlyweightNode implements DNA{
+
+    @Override
+    public String insert(String sequence) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String remove(String sequence) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String print() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String printLengths() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String printStats() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String search(String sequence) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
